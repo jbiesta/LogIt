89d353e8e3c79bc9cc0170c5f20283397e9470f1
Jo Biesta <jo@joeybdesign.co.uk>
2018-03-29 22:18:42 +0100

    Adding basic mvc startup project as a base template. Added a controller just to test it's working right in vscode.@

---
+++ LogIt.Web/Controllers/LogController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace LogIt.Web.Controllers
+{
+
+	public class LogController : Controller
+	{
+		public IActionResult Test()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
